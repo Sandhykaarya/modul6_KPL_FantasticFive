0972d649243f868c4d32fd880b6d235d2960515c
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-04-02 16:08:52 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace modul6_FantasticFive_47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
